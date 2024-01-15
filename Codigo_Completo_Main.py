d3474fe3ab3f4d7c60bf774b668a37ec722831f5
Julian de Jesus Campos Brandt <106264742+jhules3028@users.noreply.github.com>
2024-01-14 21:06:36 -0600

    Add files via upload

---
+++ Codigo_Completo_Main.py
@@ -0,0 +1,90 @@
+import flet as ft
+import pygame
+import os
+from flet import  colors
+
+
+#                              Importar la animacion   y los datos restantes de esta pagina (botones para acceso)
+from Primera_interfaz import animacion
+from Pagina_0 import Cara_1
+
+#                                                 Importar la autenticacion con Google
+from Autenticacion_Google import inicio_sesion
+
+#                                                       Importar el Menu
+from Pagina_1 import Pagina_principal
+
+def main(page: ft.Page):
+    opcion_entrada = "1"
+    with open('opcion_inicio_sesion.txt', 'w') as archivo:
+        archivo.write(opcion_entrada)
+
+
+
+    #                                                                   Icono de Usuario
+    #   Creacion del Icono y colocacion en el centro de la pagina
+    page.title = "Inicio de sesion"
+
+    icon = ft.Icon(name=ft.icons.PERSON_OUTLINED, color="transparent", size=160)
+    container_usuario = ft.Container(content=icon, width=page.width, height=100, alignment=ft.alignment.top_center)
+    page.add(container_usuario)
+
+    #Opciones de inicio de la app
+
+    #1 Para celulares
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
+    page.window_width = 700
+    page.window_height = 1000
+    page.window_full_screen = True #Solo disponible para el modo escritorio
+
+    page.add(animacion(page=page))
+
+    #Hacer un tiempo en lo que acaba la animacion y despues añadir los botones
+    pygame.time.delay(3200)
+    pygame.quit()
+
+    icon.color = colors.WHITE
+    container_usuario.update()
+
+
+
+    #                       Vamos a verificar si un usuario ya inicio sesion
+    nombre_archivo = 'Usuario.txt'
+    if os.path.exists(nombre_archivo):
+        with open(nombre_archivo, 'r') as archivo:
+            contenido = archivo.read()
+
+            if contenido !="Invitado" and contenido !="":
+                #print(contenido)
+
+
+                #                               ¡Aqui puedes agregar un boton o un saludo de bienvenida!
+
+                pygame.time.delay(500)
+                pygame.quit()
+
+                opcion_entrada = "3"
+                with open('opcion_inicio_sesion.txt', 'w') as archivo:
+                    archivo.write(opcion_entrada)
+                page.window_destroy()
+
+    #                                        Si no se ha iniciado sesion previamente, añade los demas elementos
+    # Añadir el contenido de la Cara 1
+    Cara_1(page=page)
+
+
+if __name__ == "__main__":
+    ft.app(target=main)
+
+#                                                 Maneras de entrar al Menu principal
+with open('opcion_inicio_sesion.txt', 'r') as archivo:
+    contenido = archivo.read()
+
+    if contenido=="3.5":
+        ft.app(target=inicio_sesion, port=8550, view=ft.AppView.WEB_BROWSER)
+
+    if contenido=="2" or contenido=="3":
+        ft.app(target=Pagina_principal)
+
+
